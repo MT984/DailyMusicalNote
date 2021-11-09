c9a08e82c05f1241816fb2037d14637daafa3ce2
MT984 <s180133@student.pg.edu.pl>
2021-11-09 20:56:24 +0100

    Added basic layout of main page.

---
+++ DailyMusicalNote/HistoryActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DailyMusicalNote
+{
+    [Activity(Label = "HistoryActivity")]
+    public class HistoryActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.history);
+
+            // Create your application here
+        }
+    }
+}
--- DailyMusicalNote/MainActivity.cs
+++ DailyMusicalNote/MainActivity.cs
@@ -1,11 +1,20 @@
-﻿using Android.App;
+﻿/*
+ * Designed and Created by Maciej Trocha
+ * Elektronika sem. 5
+ * Grupa 1.
+ * Jezyki Programowania Wysokiego Poziomu 2021
+ */
+
+using Android.App;
 using Android.OS;
 using Android.Runtime;
+using Android.Views;
 using AndroidX.AppCompat.App;
+using System;
 
 namespace DailyMusicalNote
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    [Activity(Label = "@string/app_name", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -14,7 +23,20 @@ namespace DailyMusicalNote
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            //Full screen
+            this.Window.AddFlags(WindowManagerFlags.Fullscreen);
+
+            //Buttons deffinitions
+            View buttonStart = FindViewById<View>(Resource.Id.button_start);
+            View buttonHistory = FindViewById<View>(Resource.Id.button_history);
+
+
+            //Buttons events handlers
+            buttonStart.Click += (s, e) => StartActivity(typeof(DifficultyActivity));
+            buttonHistory.Click += (s, e) => StartActivity(typeof(HistoryActivity));
         }
+
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
