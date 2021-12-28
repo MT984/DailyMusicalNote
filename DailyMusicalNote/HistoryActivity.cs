16a70d47bd3525465ee6c077666278536f2a72f4
MT984 <s180133@student.pg.edu.pl>
2021-12-28 18:42:02 +0100

    Added view of history page, and added mechanism to show history.

--- DailyMusicalNote/HistoryActivity.cs
+++ DailyMusicalNote/HistoryActivity.cs
@@ -21,6 +21,28 @@ namespace DailyMusicalNote
 
             //Full screen
             this.Window.AddFlags(WindowManagerFlags.Fullscreen);
+
+            //Definition od variables
+            List<string> myList = new List<string>();
+            ListView lv = FindViewById<ListView>(Resource.Id.listView1);
+
+            var directory = this.FilesDir + MyEnums.StorageFolderName;
+            var filePath = directory + "/" + MyEnums.HistoryFileName;
+            int counter = 0;
+            using (var streamReader = new System.IO.StreamReader(filePath))
+            {
+                while (!streamReader.EndOfStream)
+                {
+                    counter++;
+                    //10 spaces
+                    string myString = counter.ToString() + ".          " + streamReader.ReadLine() + " pkt          " + streamReader.ReadLine()+"%";
+                    myList.Insert(0, myString);
+                }
+            }
+
+            //Create list
+            ArrayAdapter<string> myAdapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, myList);
+            lv.Adapter = myAdapter;
         }
     }
 }
