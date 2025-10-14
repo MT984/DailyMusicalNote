323ff169c944630e594f488c6e392b89bdcd9b21
MT984 <93547556+MT984@users.noreply.github.com>
2025-10-14 23:03:46 +0200

    Added initial project files.

---
+++ DailyMusicalNote/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DailyMusicalNote
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ DailyMusicalNote/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DailyMusicalNote
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
