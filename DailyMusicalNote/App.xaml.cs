38048711da7c74b6112da230977d6d5aa9c2a877
MT984 <93547556+MT984@users.noreply.github.com>
2025-10-18 22:28:12 +0200

    Added controller class and model class.

--- DailyMusicalNote/App.xaml.cs
+++ DailyMusicalNote/App.xaml.cs
@@ -1,12 +1,21 @@
-﻿namespace DailyMusicalNote
+﻿using System.Diagnostics;
+
+namespace DailyMusicalNote
 {
     public partial class App : Application
     {
+        private readonly Model _model;
+        private readonly Controller _controller;
         public App()
         {
+            Debug.WriteLine("App() constructor");
+
             InitializeComponent();
 
-            MainPage = new AppShell();
+            _model = new();
+            _controller = new(_model);
+
+            MainPage = new NavigationPage(_controller.GetMainPage);
         }
     }
 }
---
+++ DailyMusicalNote/Controller.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyMusicalNote
+{
+    class Controller
+    {
+        private readonly MainPage _mainPage;
+        private readonly Model _model;
+        public Page GetMainPage => _mainPage;
+
+        /// <summary>
+        /// Constructor of the Controller class. In the constructor
+        /// the model and the _mainPage fields are initialized.
+        /// </summary>
+        /// <param name="model">The model of the app that
+        /// is used by the controller.</param>
+        public Controller(Model model)
+        {
+            Debug.WriteLine("Controller() constructor");
+            _model = model;
+            _mainPage = new MainPage();
+        }
+    }
+}
