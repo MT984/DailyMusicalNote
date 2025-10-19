cf28c68260f5373588ea6f8055d8dea72023513a
MT984 <93547556+MT984@users.noreply.github.com>
2025-10-20 00:09:49 +0200

    Added keyboard layout and Key class.

---
+++ DailyMusicalNote/Key.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyMusicalNote
+{
+    internal enum Notes
+    {
+        NOTE_C,
+        NOTE_CSH,
+        NOTE_D,
+        NOTE_DSH,
+        NOTE_E,
+        NOTE_F,
+        NOTE_FSH,
+        NOTE_G,
+        NOTE_GSH,
+        NOTE_A,
+        NOTE_ASH,
+        NOTE_B,
+        LAST_ELEMENT
+    }
+
+    internal enum Octaves
+    {
+        OCTAVE_0,
+        OCTAVE_1,
+        OCTAVE_2,
+        OCTAVE_3,
+        OCTAVE_4,
+        OCTAVE_5,
+        OCTAVE_6,
+        OCTAVE_7,
+        OCTAVE_8,
+        LAST_ELEMENT
+    }
+
+    internal class Key : Button
+    {
+        private readonly Octaves _octave;
+        private readonly Notes _note;
+        public Notes Note => _note;
+
+        /// <summary>
+        /// Initializes a new instance of the Key class.
+        /// Inherits from the Button class.
+        /// </summary>
+        /// <param name="note">The note value of the key (e.g., C, D, etc.).</param>
+        /// <param name="octave">The octave of the key (e.g., 1, 2, etc.).</param>
+        public Key(Notes note, Octaves octave) : base()
+        {
+            _octave = octave;
+            _note = note;
+
+            Debug.WriteLine($"Key() constructor. Note: {note.ToString()}, " +
+                            $"octave: {octave.ToString()}");
+
+            Padding = new Thickness(0, 130, 0, 0);
+            CornerRadius = 0;
+
+            //Set text on C notes to inform about octeaves.
+            if (_note == Notes.NOTE_C)
+            {
+                Text = "C" + (int)_octave;
+            }
+            else
+            {
+                Text = "";
+            }
+        }
+    }
+}
--- DailyMusicalNote/Views/GameView.xaml.cs
+++ DailyMusicalNote/Views/GameView.xaml.cs
@@ -6,13 +6,20 @@ using Android.App;
 #endif
 
 namespace DailyMusicalNote.Views;
+using Microsoft.Maui.Graphics;
+using Microsoft.Maui.Layouts;
+using static System.Net.Mime.MediaTypeNames;
 
 public partial class GameView : ContentPage
 {
+    /// <summary>
+    /// GameView() constructor. Initializes a keyboard layout.
+    /// </summary>
 	public GameView()
 	{
 		InitializeComponent();
-	}
+        GenerateKeyboard();
+    }
 
     /// <summary>
     /// OnAppearing() is invoked when the GameView becomes visible.
@@ -27,5 +34,80 @@ public partial class GameView : ContentPage
         var activity = MainActivity.Instance;
         activity.RequestedOrientation = ScreenOrientation.Landscape;
 #endif
+    }
+
+    /// <summary>
+    /// GenerateKeyboard() generates a piano keyboard in the GUI.
+    /// </summary>
+    private void GenerateKeyboard()
+    {
+        //Keys to show in GUI.
+        Key[] keys = new Key[25];
+        AbsoluteLayout KeyboardLayout = new();
+
+        //Start values.
+        int startNote = (int)Notes.NOTE_A;
+        int startOctave = (int)Octaves.OCTAVE_0;
+        double xIndex = 0;
+
+        for (int i = 0; i < keys.Length; i++)
+        {
+            //Key initialize.
+            keys[i] = new Key((Notes)startNote++, (Octaves)startOctave);
+
+            //If the last note is NOTE_B, increment the
+            //octave and start from NOTE_C.
+            if(startNote == (int)Notes.LAST_ELEMENT)
+            {
+                startNote = 0;
+                startOctave++;
+            }
+
+            //Color button based on note sharpness.
+            if ( new[] {Notes.NOTE_CSH, Notes.NOTE_DSH,
+                        Notes.NOTE_FSH, Notes.NOTE_GSH,
+                        Notes.NOTE_ASH }.Contains(keys[i].Note))
+            {
+                keys[i].Style = (Style)Microsoft.Maui.Controls.
+                                 Application.Current.Resources["KeyboardBlackKey"];
+            }
+            else
+            {
+                keys[i].Style = (Style)Microsoft.Maui.Controls.
+                                 Application.Current.Resources["KeyboardWhiteKey"];
+
+                //Position and add key to layout.
+                AbsoluteLayout.SetLayoutBounds(keys[i], new Rect(xIndex, 0, 45, 170));
+                AbsoluteLayout.SetLayoutFlags(keys[i], AbsoluteLayoutFlags.None);
+                KeyboardLayout.Children.Add(keys[i]);
+
+                //Set index to next note.
+                xIndex += 45;
+            }
+        }
+
+        //Reset index.
+        xIndex = -45;
+        for (int i = 0; i < keys.Length; i++)
+        {
+            if (keys[i].Style == (Style)Microsoft.Maui.Controls.
+                                  Application.Current.Resources["KeyboardBlackKey"])
+            {
+                //Position and add key to layout (black keys).
+                double xBuff = xIndex + 45 * 0.7; 
+                AbsoluteLayout.SetLayoutBounds(keys[i], new Rect(xBuff, 40, 30, 30));
+                AbsoluteLayout.SetLayoutFlags(keys[i], AbsoluteLayoutFlags.None);
+
+                KeyboardLayout.Children.Add(keys[i]);
+            }
+            else
+            {
+                //Set index to next note.
+                xIndex += 45;
+            }
+        }
+
+        //Add layout to "Keyboard" section.
+        Keyboard.Children.Add(KeyboardLayout);
     }
 }
