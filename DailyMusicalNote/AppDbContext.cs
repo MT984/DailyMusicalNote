5a1f88706d0172e198f9a36044fd5dfa9e713475
MT984 <93547556+MT984@users.noreply.github.com>
2025-10-30 15:47:25 +0100

    Added missing file with save data

---
+++ DailyMusicalNote/AppDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace DailyMusicalNote
+{
+    public class Save
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime dateTime { get; set; }
+        public int score { get; set; }
+        public int accuracy { get; set; }
+        public string gameplayTime { get; set; }
+        public int noteCounter { get; set; }
+        public Difficulty difficulty { get; set; }
+    }
+
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Save> Save { get; set; }
+
+        private string _dbPath = Path.Combine(FileSystem.AppDataDirectory, "save.db");
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Filename={_dbPath}");
+    }
+
+}
